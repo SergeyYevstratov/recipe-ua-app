ddfe96439c2fbbcbdb36d0c4b6c52030c3a239f4
yevst <evstratovsergey@gmail.com>
2026-01-12 17:43:39 +0200

    Fix router for GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
 import RecipeDetails from '../pages/RecipeDetails.vue'
 import Favorites from '../pages/Favorites.vue'
 import About from '../pages/About.vue'
 
-export default createRouter({
-  history: createWebHistory(),
+const router = createRouter({
+  history: createWebHashHistory(),
   routes: [
     { path: '/', component: Home },
     { path: '/recipe/:id', component: RecipeDetails, props: true },
     { path: '/favorites', component: Favorites },
     { path: '/about', component: About },
   ],
-  scrollBehavior() { return { top: 0 } }
+  scrollBehavior() {
+    return { top: 0 }
+  }
 })
+
+export default router
